be3adcd92895ace260bb29d02b0c1c46987da23f
liquidberny <bernardo.urquijo.bu@gmail.com>
2024-09-30 09:57:31 -0700

    primer commit de estructura

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <nav>
+    <a href="/">Home</a>
+    <a href="/features">Features</a>
+    <a href="/pricing">Pricing</a>
+    <a href="/contact">Contact</a>
+  </nav>
+  <main class="bg-slate-100">
+    <RouterView />
+  </main>
+</template>
+
+<script lang="ts" setup></script>
